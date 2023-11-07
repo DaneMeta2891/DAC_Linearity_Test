a2ce64e4d31223d01afdc2c2ae5645f58aa36a82
Dane Schwab <fvtuser13782@meta.com>
2023-11-07 09:56:24 -0800

    Added a scope connection utility and a short script to utilize it

---
+++ scopeTest.py
@@ -0,0 +1,36 @@
+import socket
+from socket import timeout
+
+SCOPE_IP = "169.254.89.217"
+PORT = 5025
+
+class scopeCommunincation:
+    def __init__(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.settimeout(2)
+    
+    def connect(self):
+        self.s.connect((SCOPE_IP, PORT))
+    
+    def send(self, msg):
+        self.s.send((msg + "\n").encode())
+        
+    def sendRecv(self, msg):
+        self.s.send((msg + "\n").encode())
+        try:
+            print(self.s.recv(1024).decode().rstrip("\n"))
+            return
+        except timeout:
+            print("Socket Timeout")
+            return False
+    
+    def disconnect(self):
+        self.s.close()
+
+socket = scopeCommunincation()
+socket.connect()
+inputString = input("Enter Command String: ")
+while (inputString != ""):
+    socket.sendRecv(inputString[1:]) if inputString[0].lower() == "r" else socket.send(inputString)
+    inputString = input("Enter Command String: ")
+socket.disconnect()
