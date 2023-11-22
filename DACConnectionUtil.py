216f2f22b4802f264c43cc0d83358c06bbf152f7
Dane Schwab <fvtuser13782@meta.com>
2023-11-22 15:07:21 -0800

    Developed cool_LCOS(), check_LCOS_temp(), generate_DAC_char_xlsx(). added notes about the behavior of the LCOS when enabling and disabling LC mode.

--- DACConnectionUtil.py
+++ DACConnectionUtil.py
@@ -12,11 +12,11 @@ from argparse import ArgumentParser
 #if temp value hits 60*c wait 1 min to allow the device to cool down
 #get temp-(rgb) db(DDB) or lc(LCOS), get use LCOS value for temp control
 #todo:
-#find out how to set LC mode
-#find out what causes the LCOS to heat up, and what can be disabled to expedite cooling
 #use set en-lcos=0 (1 for enable) to disable LCOS for cooling down
 #ask what lc-init does, why both lc-init and en-lcos exist
 #lc-lowc=1 (1 for low current mode, 0 for normal current mode)
+#after enabling LC mode (set lc-lowc=1) all LEDs will be set to the LC mode current corresponding to the DAC value
+#that the LED was set to in HC mode, and vice versa
 
 class DAC_Connection_Util:
     BAUDRATE = 115200
@@ -56,67 +56,87 @@ class DAC_Connection_Util:
             userCmd = input("Enter Command: ")
             if (userCmd != ""):
                 if (userCmd[0] == 'r'):
-                    self.extract_return_val(userCmd[1:])
+                    print(self.extract_return_float(userCmd[1:]))
                 else:
-                    self.send_command(userCmd)
+                    self.send_command(userCmd, False, True)
             else:
                 self.s.close()
                 break
     
-    def send_command(self, command_string, loop_until_success = False):
+    def send_command(self, command_string, loop_until_success = False, debug_flag=False):
         for attempt_num in range(5):
             self.s.flush()
             self.s.write((":" + command_string + "\r\n").encode())
 
+            command_returns = self.s.readlines()
+
+            if (debug_flag):
+                print(command_returns)
+
             #check for errors the return or continue to retry until attempt limit is reached
-            error_list = [odd_element for odd_element in self.s.readlines()[1::2] if (odd_element.decode()[-5:-2] != "ack")]
+            errors = [odd_element for odd_element in command_returns[1::2] if (odd_element.decode()[-5:-2] != "ack")]
             if not(loop_until_success):
-                return False if len(error_list) > 0 else True
-            elif (loop_until_success and len(error_list) == 0):
+                return False if len(errors) > 0 else True
+            elif (loop_until_success and len(errors) == 0):
                 return True
             else:
-                print("Unable to detect command ack, retry attempt " + str(attempt_num + 1))
+                print("ack not detected, retry..." + str(attempt_num + 1))
             
             #short delay before second attempt
-            time.sleep(2)
+            time.sleep(1)
     
     def extract_return_float(self, command_string):
         self.s.flush()
         self.s.write((":" + command_string + "\r\n").encode())
         try:
-            return_val = self.s.readlines()[0].decode()
-            print(str(type(return_val)) + ": " + str(return_val))
-            return float(return_val)
+            return self.s.readlines()[0].decode().strip()
         except:
             print("Output format not recognized")
             return -1.0
     
-    def check_LCOS_temp(self):
-        return
+    def check_LCOS_temp(self, temp_threshold=60):
+        if (float(self.extract_return_float("get temp-lc")) > temp_threshold):
+            return True
+        else:
+            return False
 
-    def cool_LCOS(self, disable_time=15):
-        #set current to LEDs to zero then disable all LEDs
+    def cool_LCOS(self, disable_time=20):
+        print("Disabling LCOS and cool for " + str(disable_time) + "seconds")
+
+        #set current to LEDs to zero
         self.send_command("set ri=0:set gi=0:set bi=0")
-        self.send_command("set a-l=0")
 
-        #disable LCOS then wait
-        
+        #disable LCOS then wait until it cools down
         self.send_command("set en_lcos=0")
         time.sleep(disable_time)
 
         #enable LCOS then wait until it's ready to receive commands again
         self.send_command("set en_lcos=1")
-        #play with this value to garuntee no error, or check for ack string in return call else retry
-        #once lcos has been re-enabled there is no need to re-able LEDs with a-l
         time.sleep(5)
-        self.send_command("set mode=5")
-
-        return
+        self.send_command("set mode=5", True)
     
+    #generates .xlsx of current (mA) values for any given DAC value
     def generate_DAC_char_xlsx(self, output_file_name):
         wb = openpyxl.Workbook()
         ws = wb.active
-        ws.append(["Enter","Column","Headers","Here"])
+        
+        #table header
+        ws.append(["","","LC_Mode","","","HC_Mode"])
+        ws.append(["DAC_Value","Red","Green","Blue","Red","Green","Blue"])
+
+        #iterate through all possible permutations to fill out spreadsheet
+        #set all LED current values to zero
+        for mode in ("LC_Mode", "HC_Mode"):
+            #set mode to LC or HC
+            #max current for LC is 31.37
+            for color in ("red", "green", "blue"):
+                #define variable to store current(mA) within loop
+                for DAC_value in range(1024):
+                    #cool LCOS if temp is above threshold
+                    if (self.check_LCOS_temp()):
+                        self.cool_LCOS()
+                    #
+
         wb.save(output_file_name + ".xlsx")
 
 DAC_Connection_Util()
