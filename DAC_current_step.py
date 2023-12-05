24c67a01890aaf77930356d96889ee31f21e4465
Dane Schwab <fvtuser13782@meta.com>
2023-12-04 16:48:20 -0800

    see prev commit msg

--- DAC_current_step.py
+++ DAC_current_step.py
@@ -1,4 +1,5 @@
 from arg_parser import arg_parser
+from DAC_connection_util import DAC_Connection_Util
 
 def main():
     test_settings = arg_parser().parse_args()
--- DAC_scope_communication.py
+++ DAC_scope_communication.py
@@ -1,8 +1,11 @@
 from scope_communication import scopeConnectionUtil
 
 class scope_control:
+    SCOPE_IP = ""
     def __init__(self):
         #establish and verify scope connection
+        self.scope_com = scopeConnectionUtil()
+        self.scope_com.connect(self.SCOPE_IP)
         return
     
     def setup_scope_measurements(self):
@@ -12,3 +15,15 @@ class scope_control:
     def get_measurement_data(self):
         #get and parse current value from measurements
         return
+    
+    def test_interface(self):
+        while (True):
+            user_cmd = input("Enter Command: ")
+            if (user_cmd != ""):
+                if (user_cmd[0] == 'r'):
+                    print()
+                else:
+                    self.send_command()
+            else:
+                self.s.close()
+                break
